651e44338125426b21f718d5240c18783207f6b4
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-12-06 15:23:07 -0600

    done

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-
 def hello(request):
-    return HttpResponse('Hello GCP!')
+    return HttpResponse('Hello! This is a Django app on GCP!!!') 
